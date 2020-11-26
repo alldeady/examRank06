d1fc79ef1ed3369fe09ae88c5e46402c6ff3d85c
Cucumber Snowball <csnowbal@ox-f1.msk.21-school.ru>
2020-11-26 14:25:37 +0300

    1

--- main.c
+++ main.c
@@ -1,78 +1,94 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 
 int extract_message(char **buf, char **msg)
 {
-    char    *new_buf;
-    int     i;
+	char	*newbuf;
+	int	i;
 
-    *msg = 0;
-    if (*buf == 0)
-        return (0);
-    i = 0;
-    while ((*buf)[i])
-    {
-        if ((*buf)[i] == '\n')
-        {
-            new_buf = calloc(1, sizeof(*new_buf) * (strlen(*buf + i + 1) + 1));
-            *msg = *buf;
-            (*msg)[i + 1] = 0;
-            *buf = new_buf;
-            return (1);
-        }
-        i++;
-    }
-    return (0);
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
-//char *str_join(char *buf, char *add)
-
-int main()
+char *str_join(char *buf, char *add)
 {
-    int sock_fd, conn_fd, len;
-    struct sockaddr_in servaddr, cli;
+	char	*newbuf;
+	int		len;
 
-    //Socket creation and verification
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd == -1)
-    {
-        printf("Socket creation failed...\n");
-        exit(0);
-    }
-    else
-    {
-        printf("Socket succesfully created...\n");
-    }
-    bzero(&servaddr, sizeof(servaddr));
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
 
-    //Assign ip port
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(2130706433);
-    servaddr.sin_port = htons(8081);
 
-    if ((bind(sock_fd, (const struct sockaddr *) &servaddr, sizeof(servaddr)))!= 0)
-    {
-        printf("Socket binding failed...\n");
-        exit(0);
-    }
+int main() {
+	int sockfd, connfd, len;
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(8081); 
+  
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n");
+	if (listen(sockfd, 10) != 0) {
+		printf("cannot listen\n"); 
+		exit(0); 
+	}
+	len = sizeof(cli);
+	connfd = accept(sockfd, (struct sockaddr *)&cli, &len);
+	if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
     else
-        printf("Socket succesfully binded...\n");
-    if (listen(sock_fd, 10) != 0)
-    {
-        printf("Cant listen...\n");
-        exit(0);
-    }
-    len = sizeof(cli);
-    conn_fd = accept(sock_fd, (struct sockaddr *)&cli, &len);
-    if (conn_fd < 0)
-    {
-        printf("Failed to accept...\n");
-        exit(0);
-    }
-    else
-        printf("Server accept the client...\n");
+        printf("server acccept the client...\n");
 }
